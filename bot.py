d30e6512d08498015990756b7079dac8e1c00b48
Deividjesy <deividjesy@gmail.com>
2020-05-23 02:24:21 -0700

    Archivos de bot

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import discord
-
+from datetime import datetime
 id = 713590828056051774
 
 def read_token():
@@ -28,7 +28,15 @@ async def on_message(message):
         elif message.content == "!user":
             await message.channel.send(f"""Usuarios en el server: {id.member_count} """)
         elif message.content == "!mytag":
-            await message.channel.send(f"""id: {message.author}  """)
+            await message.channel.send(f"""id ;:v: {message.author}  """)
+        elif message.content == "!fecha":
+            now = datetime
+            if now.hour() == 1 and now.minute() == 52:
+                message = 'a'
+                await message.channel.send(message)
+            time=90
+        else:
+            time=1
 
 
 
